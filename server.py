c5d1b391c86d296449f5011c9f07c00497915424
Bhavya <76608764+Bhavya202@users.noreply.github.com>
2023-07-27 22:13:27 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
-import socket;
-from threading import Thread;
-import random;
+import socket
+from threading import Thread
+import random
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip_address = '127.0.0.1';
-port = 5500;
+ip_address = '127.0.0.1'
+port = 8000
 
-server.bind((ip_address, port));
-server.listen();
+server.bind((ip_address, port))
+server.listen()
 
-list_of_clients = [];
+list_of_clients = []
+nicknames = []
 
 questions = [
      " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.Patty\n d.Pizza",
@@ -30,51 +31,63 @@ questions = [
      " Hg stands for? \n a.Mercury\n b.Hulgerium\n c.Argenine\n d.Halfnium",
      " Who gifted the Statue of Libery to the US? \n a.Brazil\n b.France\n c.Wales\n d.Germany",
      " Which planet is closest to the sun? \n a.Mercury\n b.Pluto\n c.Earth\n d.Venus"
-];
+]
 
-answers = ['d', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 'c', 'b', 'd', 'd', 'c', 'a', 'b', 'a'];
-print("Server has started...");
+answers = ['d', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 'c', 'b', 'd', 'd', 'c', 'a', 'b', 'a']
+
+print("Server has started...")
 
 def get_random_question_answer(conn):
-    random_index = random.randint(0,len(questions) - 1);
-    random_question = questions[random_index];
-    random_answer = answers[random_index];
-    conn.send(random_question.encode('utf-8'));
-    return random_index, random_question, random_answer;
+    random_index = random.randint(0,len(questions) - 1)
+    random_question = questions[random_index]
+    random_answer = answers[random_index]
+    conn.send(random_question.encode('utf-8'))
+    return random_index, random_question, random_answer
 
 def remove_question(index):
-    questions.pop(index);
-    answers.pop(index);
+    questions.pop(index)
+    answers.pop(index)
 
-def clientthread(conn):
-    score = 0;
-    conn.send("Welcome to this quiz game!".encode('utf-8'));
-    conn.send("You will receive a question. The answer to that question should be one of a, b, c or d\n".encode('utf-8'));
-    conn.send("Good Luck!\n\n".encode('utf-8'));
-    index, question, answer = get_random_question_answer(conn);
+def clientthread(conn, nickname):
+    score = 0
+    conn.send("Welcome to this quiz game!".encode('utf-8'))
+    conn.send("You will receive a question. The answer to that question should be one of a, b, c or d!\n".encode('utf-8'))
+    conn.send("Good Luck!\n\n".encode('utf-8'))
+    index, question, answer = get_random_question_answer(conn)
+    print(answer)
     while True:
         try:
-            message = conn.recv(2048).decode('utf-8');
+            message = conn.recv(2048).decode('utf-8')
             if message:
-                if message.lower() == answer:
-                    score += 1;
-                    conn.send(f"Bravo! Your score is {score}\n\n".encode('utf-8'));
+                if message.split(": ")[-1].lower() == answer:
+                    score += 1
+                    conn.send(f"Bravo! Your score is {score}\n\n".encode('utf-8'))
                 else:
-                    conn.send("Incorrect answer! Better luck next time!\n\n".encode('utf-8'));
-                remove_question(index);
-                index, question, answer = get_random_question_answer(conn);
+                    conn.send("Incorrect answer! Better luck next time!\n\n".encode('utf-8'))
+                remove_question(index)
+                index, question, answer = get_random_question_answer(conn)
+                print(answer)
             else:
-                remove(conn);
-        except:
+                remove(conn)
+                remove_nickname(nickname)
+        except Exception as e:
+            print(str(e))
             continue
 
 def remove(connection):
     if connection in list_of_clients:
-        list_of_clients.remove(connection);
+        list_of_clients.remove(connection)
+
+def remove_nickname(nickname):
+    if nickname in nicknames:
+        nicknames.remove(nickname)
 
 while True:
-    conn, addr = server.accept();
-    list_of_clients.append(conn);
-    print (addr[0] + " connected");
-    new_thread = Thread(target=clientthread, args=(conn,));
-    new_thread.start();
+    conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
+    list_of_clients.append(conn)
+    nicknames.append(nickname)
+    print (nickname + " connected!")
+    new_thread = Thread(target= clientthread,args=(conn,nickname))
+    new_thread.start()
